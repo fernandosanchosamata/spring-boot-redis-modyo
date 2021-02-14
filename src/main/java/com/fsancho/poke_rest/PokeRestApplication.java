4d3f7210dacd6647d28b4d12034b58dadff66176
fernando sancho <fernandosanchosamata@gmail.com>
2021-02-14 13:28:42 -0300

    subiendo proyecto inicial

---
+++ src/main/java/com/fsancho/poke_rest/PokeRestApplication.java
@@ -0,0 +1,13 @@
+package com.fsancho.poke_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeRestApplication.class, args);
+	}
+
+}
