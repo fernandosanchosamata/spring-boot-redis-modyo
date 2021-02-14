515a8be1a1a89b1430d21ea308039a606bb446ca
fernando sancho <fernandosanchosamata@gmail.com>
2021-02-14 13:56:35 -0300

    test spring boot

--- src/main/java/com/fsancho/poke_rest/PokeRestApplication.java
+++ src/main/java/com/fsancho/poke_rest/PokeRestApplication.java
@@ -2,6 +2,8 @@ package com.fsancho.poke_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class PokeRestApplication {
@@ -9,5 +11,11 @@ public class PokeRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PokeRestApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 }
