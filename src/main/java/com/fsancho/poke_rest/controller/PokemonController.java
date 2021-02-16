df14f905016d6a0b1030c05df72506df2ea3e5cf
fernando sancho <fernandosanchosamata@gmail.com>
2021-02-16 08:46:45 -0300

    Continuan pruebas con redis

--- src/main/java/com/fsancho/poke_rest/controller/PokemonController.java
+++ src/main/java/com/fsancho/poke_rest/controller/PokemonController.java
@@ -1,6 +1,5 @@
 package com.fsancho.poke_rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -10,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,17 +25,14 @@ public class PokemonController {
     @Autowired
     private PokemonService pokemonService;
 
-    @GetMapping(value = "/pokemones/{offset}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/pokemones", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<?> getAll(@PathVariable String offset,
+    public ResponseEntity<?> getAll(@RequestParam(value = "offset", required = true) String offset,
             @RequestParam(value = "limit", required = true) String limit) {
         try {
-            int intOffSet = Integer.valueOf(offset);
-            int intLimit = Integer.valueOf(limit);
 
-            List<PokemonDTO> productos = pokemonService.searchPokemonLazy(intOffSet, intLimit);
-            
-            logger.info("There are products to list");
+            List<PokemonDTO> productos = pokemonService.searchPokemonLazy(offset, limit);
+            logger.info("Devolviendo pokemones");
             return new ResponseEntity<>(productos, HttpStatus.OK);
 
         } catch (Exception e) {
@@ -45,20 +40,4 @@ public class PokemonController {
             return new ResponseEntity<List<PokemonDTO>>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
-    @RequestMapping("/pokemones")
-    @ResponseBody
-    public List<PokemonDTO> getAll() {
-        try {
-            int intOffSet = Integer.valueOf(0);
-            int intLimit = Integer.valueOf(0);
-
-            List<PokemonDTO> productos = pokemonService.searchPokemonLazy(intOffSet, intLimit);
-            return productos;
-
-        } catch (Exception e) {
-            return new ArrayList<>();
-        }
-    }
-
 }
--- src/main/java/com/fsancho/poke_rest/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/fsancho/poke_rest/service/impl/PokemonServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fsancho.poke_rest.config.redis.CacheConfig;
 import com.fsancho.poke_rest.model.PokemonDTO;
 import com.fsancho.poke_rest.model.PokemonResponse;
 import com.fsancho.poke_rest.service.PokemonService;
@@ -28,42 +26,35 @@ import com.fsancho.poke_rest.service.PokemonService;
 @Service
 public class PokemonServiceImpl implements PokemonService{
     
-    private static final String OMDB_MIME_TYPE = "application/json";
-    private static final String OMDB_API_BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+    private static final String API_BASE_URL = "https://pokeapi.co/api/v2/pokemon";
     private static final Logger logger = LoggerFactory.getLogger(PokemonServiceImpl.class);
     
     @Autowired
     RestTemplate restTemplate;
 
-    public PokemonServiceImpl() {
-        
-    }
-
-    @Cacheable(cacheNames = CacheConfig.USER_CACHE, unless = "#result == null")
+    // @Cacheable(cacheNames = CacheConfig.USER_CACHE, unless = "#result ==
+    // null")
     @Override
-    public List<PokemonDTO> searchPokemonLazy(int offset, int limit) {
-        String url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-
-        ResponseEntity<PokemonResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity,
-                PokemonResponse.class);
+    public List<PokemonDTO> searchPokemonLazy(String offset, String limit) {
+        logger.info("Obteniendo pokemones desde api - logger");
+        System.out.println("Obteniendo pokemones desde api - systemout");
+        String url = API_BASE_URL + "?limit=" + limit + "&offset=" + offset;
+        HttpEntity<String> entity = getHttpEntity();
+        ResponseEntity<PokemonResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity, PokemonResponse.class);
         PokemonResponse results = response.getBody();
-        // Map<String, List<PokemonDTO>> geonames =
-        // restTemplate.getForObject(url, Map.class);
-        //
-        // List<PokemonDTO> data = geonames.get("results");
-
         return results.getResults();
     }
 
     @Override
     public PokemonDTO findPokemonById(int id) {
         return  null;
+    }
+
+    private HttpEntity<String> getHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        return new HttpEntity<>("parameters", headers);
     }
 
 }
